ccd6716b031ccaa0663c2c45cc21f4590faa4039
Ultraligmanigma <cansuelguelfan@gmail.com>
2025-12-31 23:01:45 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,5 +2,8 @@
 const supabaseUrl = 'https://knyzvdnpruuvvlmubjrh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtueXp2ZG5wcnV1dnZsbXVianJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxOTYzNjAsImV4cCI6MjA4Mjc3MjM2MH0.KC6W1ZrGRYbvJqL57c00G6j88jDHzEXlFdqLEoZtHdM';
 
-// Initialisierung über das CDN (wird in index.html geladen)
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+// Initialisierung
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+// Export für die index.html
+window.supabaseClient = _supabase;
