e94a9bfa928e7a8cc19a983ebbbfeddf6b1c80de
Ultraligmanigma <cansuelguelfan@gmail.com>
2025-12-31 23:06:40 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 // Pufferfish Database Bridge
 const supabaseUrl = 'https://knyzvdnpruuvvlmubjrh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtueXp2ZG5wcnV1dnZsbXVianJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxOTYzNjAsImV4cCI6MjA4Mjc3MjM2MH0.KC6W1ZrGRYbvJqL57c00G6j88jDHzEXlFdqLEoZtHdM';
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
-// Initialisierung
-const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
-
-// Export für die index.html
-window.supabaseClient = _supabase;
+// Funktion, um den Login-Status überall zu prüfen
+async function checkAuth(redirectTo) {
+    const { data } = await supabase.auth.getUser();
+    if (!data.user && redirectTo) window.location.href = 'login.html';
+    if (data.user && !redirectTo) window.location.href = 'index.html';
+    return data.user;
+}
